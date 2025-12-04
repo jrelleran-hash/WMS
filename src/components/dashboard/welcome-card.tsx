aa255c1cfb1e6c2cc626ebe0ef3a0e4b029bb077
John Russelle Elleran <jrelleran@modularmajestics.com>
2025-12-04 03:45:40 +0000

    THIS IS ONLY VISIBLE TO THE USER THAT HAS ADMIN ROLE AND USERS THAT HAS

--- src/components/dashboard/welcome-card.tsx
+++ src/components/dashboard/welcome-card.tsx
@@ -5,12 +5,13 @@ import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useAuth } from "@/hooks/use-auth";
-import { Input } from "../ui/input";
-import { Search, Wrench } from "lucide-react";
+import { Wrench } from "lucide-react";
+import { useAuthorization } from "@/hooks/use-authorization";
 
 export function WelcomeCard() {
   const { userProfile } = useAuth();
   const [currentDate, setCurrentDate] = useState("");
+  const { canView } = useAuthorization({ page: '/tool-booking' });
 
   useEffect(() => {
     setCurrentDate(
@@ -42,12 +43,14 @@ export function WelcomeCard() {
         <p className="text-muted-foreground">{currentDate}</p>
       </div>
        <div className="flex items-center gap-2 w-full sm:w-auto sm:max-w-md flex-1 justify-end">
-        <Link href="/tool-booking" className="flex-initial">
-            <Button variant="outline" className="w-full sm:w-auto">
-                <Wrench className="mr-2 h-4 w-4" />
-                Tool Booking
-            </Button>
-        </Link>
+        {canView && (
+            <Link href="/tool-booking" className="flex-initial">
+                <Button variant="outline" className="w-full sm:w-auto">
+                    <Wrench className="mr-2 h-4 w-4" />
+                    Tool Booking
+                </Button>
+            </Link>
+        )}
         <Link href="/analytics" className="flex-initial">
           <Button className="w-full sm:w-auto">View Analytics</Button>
         </Link>
